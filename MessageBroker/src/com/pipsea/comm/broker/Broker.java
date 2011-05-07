0819048277e2431e22655859e510f49e7b448d4b
Raz <raz.lachyani@gmail.com>
2011-05-08 00:44:28 +0300

    Added control-C interrupt support

--- MessageBroker/src/com/pipsea/comm/broker/Broker.java
+++ MessageBroker/src/com/pipsea/comm/broker/Broker.java
@@ -203,6 +203,15 @@ public class Broker extends ThreadPoolExecutor {
         System.out.println("Closing resources ...");
         closeResources();
         System.out.println("resources are all closed ...");
+
+        // Walk up all the way to the root thread group
+        ThreadGroup rootGroup = Thread.currentThread().getThreadGroup();
+        ThreadGroup parent;
+        while ((parent = rootGroup.getParent()) != null) {
+            rootGroup = parent;
+        }
+
+        listThreads(rootGroup, "");
     }
 
     private static void closeResources(){
@@ -210,6 +219,30 @@ public class Broker extends ThreadPoolExecutor {
         frontend.close();
         backend.close();
         context.term();
+    }
+
+    public static void listThreads(ThreadGroup group, String indent) {
+        System.out.println(indent + "Group[" + group.getName() +
+                        ":" + group.getClass()+"]");
+        int nt = group.activeCount();
+        Thread[] threads = new Thread[nt*2 + 10]; //nt is not accurate
+        nt = group.enumerate(threads, false);
+
+        // List every thread in the group
+        for (int i=0; i<nt; i++) {
+            Thread t = threads[i];
+            System.out.println(indent + "  Thread[" + t.getName()
+                        + ":" + t.getClass() + "]");
+        }
+
+        // Recursively list all subgroups
+        int ng = group.activeGroupCount();
+        ThreadGroup[] groups = new ThreadGroup[ng*2 + 10];
+        ng = group.enumerate(groups, false);
+
+        for (int i=0; i<ng; i++) {
+            listThreads(groups[i], indent + "  ");
+        }
     }
 }
 
